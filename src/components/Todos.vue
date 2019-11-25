48c84b65ba64698e1cabb4acdb5ca70d2fee8f38
nprofilovic <nprofilovic@gmail.com>
2019-11-25 13:04:01 +0100

    Eslint correct

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,44 +1,44 @@
 <template>
-    <div>
-        <h1>Todos</h1>
-        <div class="legend">
-            <span>Double click to mark as complete</span>
-            <span>
-                <span class="incomplete-box"></span> = Inomplete
-            </span>
-            <span>
-                <span class="complete-box"></span> = Complete
-            </span>
-        </div>
-        <div class="todos">
-            <div @dblclick="onDblClick(todo)" v-for="todo in allTodos" :key="todo.id" class="todo" v-bind:class="{'is-complete': todo.completed}">
-                {{ todo.title }}
-                <i class="fas fa-trash-alt" @click="deleteTodo(todo.id)"></i>
-            </div>
-        </div>
+  <div>
+    <h1>Todos</h1>
+    <div class="legend">
+      <span>Double click to mark as complete</span>
+      <span>
+        <span class="incomplete-box" /> = Inomplete
+      </span>
+      <span>
+        <span class="complete-box" /> = Complete
+      </span>
     </div>
+    <div class="todos">
+      <div v-for="todo in allTodos" :key="todo.id" :class="{'is-complete': todo.completed}" class="todo" @dblclick="onDblClick(todo)">
+        {{ todo.title }}
+        <i class="fas fa-trash-alt" @click="deleteTodo(todo.id)" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import {mapGetters, mapActions} from 'vuex'
 
 export default {
-    name: "Todos",
-    methods: {
-        ...mapActions(['fetchTodos', 'deleteTodo', 'updateTodo']), 
-        onDblClick (todo) {
-            const updTodo = {
-                id: todo.id,
-                title: todo.title,
-                completed: !todo.completed
-            }
-            this.updateTodo(updTodo)
-        }
-    },
-    computed: mapGetters(['allTodos']),
-    created(){
-        this.fetchTodos();
+  name: "Todos",
+  computed: mapGetters(['allTodos']),
+  created(){
+    this.fetchTodos();
+  },
+  methods: {
+    ...mapActions(['fetchTodos', 'deleteTodo', 'updateTodo']), 
+    onDblClick (todo) {
+      const updTodo = {
+        id: todo.id,
+        title: todo.title,
+        completed: !todo.completed
+      }
+      this.updateTodo(updTodo)
     }
+  },
 }
 </script>
 
